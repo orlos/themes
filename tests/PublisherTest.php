56447800b2c7bb655b5b85525de31c7ffa672b08
Robin Radic <robin@radic.nl>
2015-08-01 07:50:05 +0200

    added publisher tests

--- tests/PublisherTest.php
+++ tests/PublisherTest.php
@@ -7,6 +7,7 @@
  */
 namespace Caffeinated\Tests\Themes;
 
+use Caffeinated\Themes\Publisher;
 use Mockery as m;
 
 /**
@@ -21,14 +22,50 @@ use Mockery as m;
  */
 class PublisherTest extends TestCase
 {
+    /** @var \Mockery\MockInterface */
+    protected $fs;
+
+    /** @var \Mockery\MockInterface */
+    protected $theme;
+
+    /** @var \Caffeinated\Themes\Publisher */
+    protected $publisher;
+
     public function setUp()
     {
         parent::setUp();
+        $this->fs        = m::mock('Illuminate\Filesystem\Filesystem');
+        $this->theme     = m::mock('Caffeinated\Themes\Theme');
+        $this->publisher = new Publisher($this->fs);
+        $this->publisher->toTheme($this->theme);
     }
 
-    public function testMe()
+    public function testPublishPackage()
     {
-        $this->assertTrue(true);
+        $name        = 'testvendor/testpkg';
+        $sourcePath  = __DIR__ . '/fixture/public/themes/frontend/default';
+        $destination = 'public/themes/frontend/default/packages/testvendor/testpkg';
+        $this->theme->shouldReceive('getCascadedPath')->once()->with(m::mustBe('packages'), m::mustBe($name))->andReturn($destination);
+        $this->fs->shouldReceive('exists')->once()->with(m::mustBe($destination))->andReturn(true);
+        $this->fs->shouldReceive('copyDirectory')->once()->with(m::mustBe($sourcePath), m::mustBe($destination))->andReturn();
+        $this->publisher
+            ->asPackage($name)
+            ->from($sourcePath)
+            ->publish();
+    }
+
+    public function testPublishNamespace()
+    {
+        $name        = 'nstest';
+        $sourcePath  = __DIR__ . '/fixture/public/themes/frontend/default';
+        $destination = 'public/themes/frontend/default/namespaces/' . $name;
+        $this->theme->shouldReceive('getCascadedPath')->once()->with(m::mustBe('namespaces'), m::mustBe($name))->andReturn($destination);
+        $this->fs->shouldReceive('exists')->once()->with(m::mustBe($destination))->andReturn(true);
+        $this->fs->shouldReceive('copyDirectory')->once()->with(m::mustBe($sourcePath), m::mustBe($destination))->andReturn();
+        $this->publisher
+            ->asNamespace($name)
+            ->from($sourcePath)
+            ->publish();
     }
 
 }
